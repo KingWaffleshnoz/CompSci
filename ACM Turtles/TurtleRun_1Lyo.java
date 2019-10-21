013c268a7edf2f608c089dbf407a52d9f0249981
Jared Lyon <jaredalyon@gmail.com>
2019-10-21 09:14:59 -0700

    acm turtles

---
+++ ACM Turtles/TurtleRun_1Lyo.java
@@ -0,0 +1,21 @@
+//Jared Lyon
+
+import acm.program.GraphicsProgram;
+//import graphics window
+import acm.graphics.GTurtle;
+//import GTurtle
+import java.awt.Color;
+//import Java color class for pens
+
+/**
+ * Jared Lyon
+ * Period 1
+ * Assignment
+ * This program builds a racetrack for three turtles
+ */
+
+ public class TurtleRun_1Lyo extends Graphics Program {
+     public static void main(String[] args) {
+         
+     }
+ }
