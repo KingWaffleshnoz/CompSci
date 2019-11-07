580ab4b7b4390b90c1c861df8d508abb047a8e0c
Jared Lyon <jaredalyon@gmail.com>
2019-11-07 09:06:10 -0800

    Create QuizzerTester_1KovLyo.java

---
+++ QuizzerTester_1KovLyo.java
@@ -0,0 +1,34 @@
+import java.util.Math;
+import java.util.Scanner;
+
+public class QuizzerTester_1KovLyo {
+    public static void main(String[] args) {
+        Scanner inputName = new Scanner(System.in);
+        String username = "";
+        System.out.println("Welcome to the KovLyo Inc. National Intelligence Test!\nEnter name:");
+        username = inputName.next();
+        Quiz quiz = new Quiz(username);
+        quiz.askQuestions();
+    }
+}
+
+class Quiz {
+    private int score = 0;
+    private String name;
+    Scanner input = new Scanner(System.in);
+
+    //constructor
+    public Quiz(String name) {
+        this.name = name;
+    }
+
+    //mutator - questions
+    public void askQuestions() {
+
+    }
+
+    //accessor
+    public String getScore() {
+        return name + "'s current score: " + score;
+    }
+}
