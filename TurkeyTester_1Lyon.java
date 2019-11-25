dfe7b1a48f9d33b88c161c62830e77b474bc583d
Jared Lyon <jaredalyon@gmail.com>
2019-11-24 16:25:00 -0800

    Create TurkeyTester_1Lyon.java

---
+++ TurkeyTester_1Lyon.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class TurkeyTester_1Lyon {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the course creator!\nInput class name:");
+        Scanner input = new Scanner(System.in);
+        String name = "";
+        name = input.next();
+        Course obj = new Course(name);
+        obj.enroll();
+        System.out.println(obj.toString());
+
+        boolean done = false;
+        while (!done) {
+            System.out.println("Search index:");
+            int get = input.nextInt();
+            System.out.println(obj.get(get));
+            
+            System.out.println("Search name:");
+            String find = "";
+            find = input.next();
+            System.out.println(obj.find(find));
+
+            //done?
+            System.out.println("Continue? [1]");
+            int yes = input.nextInt();
+                if (yes == 1) {
+                    done = false;
+                } else {
+                    System.out.println("Exiting...");
+                    done = true;
+                }
+        }
+    }
+}
+
+class Course {
+    private String[] students;
+    private String name;
+    private int temp;
+
+    //Constructor
+    public Course(String name) {
+        this.name = name;
+    }
+
+    //Accessors
+    public String get(int index) {
+        String x = students[index];
+        return x;
+    }
+
+    public int find(String name) {
+        int len = students.length;
+        for (int i = 0; i < len; i++) {
+            if (students[i].equals(name)) {
+                temp = i;
+            } else {
+                temp = 0;
+            }
+        }
+        return temp;
+    }
+
+    //Mutator
+    public void enroll() {
+        System.out.println("Input class size (enter '0' to autogenerate class):");
+        Scanner inputSize = new Scanner(System.in);
+        int size = inputSize.nextInt();
+
+        if (size == 0) {
+            students = new String[] { "Barry", "Larry", "Garry", "Terry", "Carrie", "Robert", "Bert", "Rob", "Bob", "Robbie" };
+        } else {
+            students = new String[size];
+            for (int i = 0; i < size; i++) {
+                System.out.println("Input student " + i + "'s name:");
+                Scanner inputStudent = new Scanner(System.in);
+                students[i] = inputStudent.next();
+            }
+        }
+    }
+
+    //toString
+    public String toString() {
+        String output = "Course: " + name + "\n" + Arrays.toString(students);
+        return output;
+    }
+}
